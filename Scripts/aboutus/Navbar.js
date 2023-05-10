25a35a2295cda6858b141fd8707457d71ab9be21
Douglas Brandão <107072536+Jdouglasbrandao@users.noreply.github.com>
2023-05-10 15:20:33 -0300

    Commit inicial
    Commit inicial da página.

---
+++ Scripts/aboutus/Navbar.js
@@ -0,0 +1,9 @@
+/* ----------Developed by JDouglasbrandao------------ */
+let menu = document.querySelector("#menu-icon");
+let Navbar = document.querySelector(".Navbar");
+
+menu.onclick =()=>{
+    menu.classList.toggle('bx-x');
+    Navbar.classList.toggle('open');
+}
+/* ----------Developed by JDouglasbrandao------------ */
---
+++ Scripts/index/scrollreveal.js
@@ -0,0 +1,8 @@
+/* ----------Developed by JDouglasbrandao------------ */
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.effect',{rotate: {x: 0, y:80, z:0},  duration: 2000})
+
+sr.reveal('.effect2',{rotate: {x: 0, y:80, z:0},  duration: 2000})
+
+sr.reveal('.effect3',{rotate: {x: 0, y:80, z:0},  duration: 2000})
+/* ----------Developed by JDouglasbrandao------------ */
